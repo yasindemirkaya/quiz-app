d572b0afe7976e996c283f53ab4120636afd9c30
yasindemirkaya <yasin97demirkaya@gmail.com>
2023-05-08 16:47:50 +0300

    QA-1
    Turnuva quiz önyüz tamamlandı

---
+++ pages/tournament/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <TournamentQuiz />
+  </div>
+</template>
+  
+  <script>
+  import TournamentQuiz from '~/components/tournament/TournamentQuiz.vue'
+  export default {
+    name: 'TournamentPage',
+    components: {
+      TournamentQuiz
+    }
+  }
+  </script>
